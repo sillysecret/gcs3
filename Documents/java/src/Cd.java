538bfe198d73100ba7cb8ede2ca27398076e6f50
Joao Pedro Da Silva Kriger <23108522@facin.portoalegre.pucrsnet.br>
2023-09-01 18:57:13 -0300

    git commit

---
+++ Documents/java/src/Cd.java
@@ -0,0 +1,18 @@
+public class Cd extends item{
+    
+    private String genero; 
+    private String nomemsc;
+
+
+    //@Override
+    public Cd(String genero,String nomemscString){
+        
+        
+        
+        this.genero=genero;
+        
+        this.nomemsc = nomemsc;
+    }
+
+
+}
