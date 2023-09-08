9956c01b191fb9890e4181c062903b85a12b525a
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2023-09-08 18:26:14 -0300

    feat: add class livro

---
+++ Documents/java/src/Livro.java
@@ -0,0 +1,11 @@
+public class Livro extends item {
+
+        private String nomeeditora;
+        private int anopublicacao;
+
+        public Livro(String nomeeditora,int anopublicacao){
+            this.nomeeditora=nomeeditora;
+
+            this.anopublicacao = anopublicacao;
+        }
+}
