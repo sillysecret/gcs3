9d19a6ddea938c5f1789f236d02555a17f8521d1
Joao Pedro Da Silva Kriger <23108522@facin.portoalegre.pucrsnet.br>
2023-09-08 18:10:21 -0300

    git commit

--- Documents/java/src/Cd.java
+++ Documents/java/src/Cd.java
@@ -4,13 +4,10 @@ public class Cd extends item{
     private String nomemsc;
 
 
-    //@Override
-    public Cd(String genero,String nomemscString){
-        
-        
-        
+    //@Override 
+    public Cd(String genero,String nomemscString,String nome, String data,  String autores){
+        super(nome, data, autores); 
         this.genero=genero;
-        
         this.nomemsc = nomemsc;
     }
 
