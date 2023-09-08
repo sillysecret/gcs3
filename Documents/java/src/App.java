2d7c28cfa2fe7c6422f9f51974f75d70dfc77bc1
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2023-09-08 18:26:21 -0300

    feat: add class livro

--- Documents/java/src/App.java
+++ Documents/java/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Cd> cds = new ArrayList<Cd>();
+        //ArrayList<Dvd> dvds = new ArrayList<Dvd>();
+        ArrayList<Livro> livros = new ArrayList<Livro>();
+
+
     }
 }
